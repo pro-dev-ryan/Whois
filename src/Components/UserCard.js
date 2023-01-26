3a92f25913990e926edf77c09dc1a36e0a1d29c9
pro-dev-ryan <azizul.developer@gmail.com>
2023-01-26 18:20:54 +0600

    card component has been created

---
+++ src/Components/UserCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserCard = () => {
+  return (
+    <div>
+      <p>This is card</p>
+    </div>
+  );
+};
+
+export default UserCard;
---
+++ src/Sections/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p className="text-center">All rights reserved &copy; &reg; 2023</p>
+    </div>
+  );
+};
+
+export default Footer;
